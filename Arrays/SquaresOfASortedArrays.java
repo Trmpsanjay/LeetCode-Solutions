1dbbe0658781e726232342a4959e8a6db3f038d8
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-06-06 00:13:26 +0530

    Create SquaresOfASortedArrays.java

---
+++ Arrays/SquaresOfASortedArrays.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        for(int i=0;i<A.length;i++){
+            A[i]=A[i]*A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
