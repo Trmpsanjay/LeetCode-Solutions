d1670882240ce58a1b48a907ed6e2882ecb86b76
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-12-26 23:28:50 +0530

    o(n) solution addded

--- maximumProductOfArray.java
+++ maximumProductOfArray.java
@@ -1,7 +1,27 @@
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         Arrays.sort(nums);
+//         return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+        
+//     }
+// }
+
+
+//method 2 : find two bigest num
 class Solution {
     public int maxProduct(int[] nums) {
-        Arrays.sort(nums);
-        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+        int max = Integer.MIN_VALUE;
+        int maxLow = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                maxLow = max;
+                max = nums[i];
+            }else if(nums[i]>maxLow){
+                maxLow = nums[i];
+            }
+        }
+        return (max-1)*(maxLow-1);
+        
         
     }
 }
